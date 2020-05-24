f9c2454ac85b4eaaebe56723e5489b35be1977d4
Technical Arslan <65797495+technicalarslan@users.noreply.github.com>
2020-05-24 10:37:44 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
+|__(~)    •||•Arslan - Baloch -OFFICAL------•◈•
 |__\~~) •||•RANA - RAJPUT---------------•◈•
 |__(-----\  •◈•------BLACK-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;Arslan786
+\033[1;96mWhatsApp \033[1;93m: \033[1;+923013505332
+\033[1;96mFacebook  \033[1;93m: \033[1; Arslan Baloch
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/technicalarslan/blank
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mArslanBaloch
+\033[1;96mWhatsApp \033[1;93m: \033[1;92m03013505332
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Arslan Baloch
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/technicalarslan/blank
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Arslan"
+CorrectPassword = "Baloch"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open ')https://www.youtube.com/channel/UCO7xGJq5IADddwq74P-VutA
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open ')https://www.youtube.com/channel/UCO7xGJq5IADddwq74P-VutA
 
 def login():
 	os.system('clear')
